6b9122e7659e3dd11a74e665c5f3afc3cb3e083c
Ovoyana <olyakovbas@gmail.com>
2024-06-19 17:32:58 +0200

    attached files

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,24 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+    if (err instanceof HttpError) {
+        res.status(err.status).json({
+          status: err.status,
+          message: err.name,
+          data: err,
+        });
+        return;
+      }
+
+    // res.json({
+    //     status: 500,
+    //     message: 'Something went wrong',
+    //     data: err.message,
+    // });
+
+    res.json({
+        status:404,
+        message: 'Contact not found',
+        data: err.message,
+    })
+  };
